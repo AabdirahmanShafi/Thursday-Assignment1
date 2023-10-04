d620e729ba5d3f0fd560ff7a458287adcbb20df0
AabdirahmanShafi <117932483+AabdirahmanShafi@users.noreply.github.com>
2023-10-04 18:03:26 +0300

    Add files via upload

---
+++ Assignment 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Welcome to the wonderful world of C++!""\n";
+    cout<<"This is my first C++ output statement""\n";
+    cout<<"Soon, I will learn how to input data into my""\n";
+    cout<<"program";
+
+    return 0;
+    
+}
